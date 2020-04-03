b5bbee012f8eccf525b62d9d3968d356af5b8bbc
xikaiwen <xikw3927@gmail.com>
2020-04-03 13:22:10 +0800

    UserDao中方法验证

--- src/main/java/com/xiaobin/web/controller/DataController.java
+++ src/main/java/com/xiaobin/web/controller/DataController.java
@@ -1,6 +1,7 @@
 package com.xiaobin.web.controller;
 
 import com.xiaobin.annotation.DataSource;
+import com.xiaobin.web.dao.UserDao;
 import com.xiaobin.web.entity.User;
 import com.xiaobin.web.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,9 @@ public class DataController {
     @Autowired
     private IUserService userService;
 
+    @Autowired
+    private UserDao userDao;
+
     /**
      * 一定要加DataSource注解不然切面获取不到
      */
@@ -35,5 +39,17 @@ public class DataController {
     public String getSlaveName(@PathVariable Integer id){
         User user = userService.findUserName(id);
         return user.getName();
+    }
+
+    @DataSource(name = "data-slave")
+    @GetMapping("queryAllWithSlave")
+    public Object queryAllWithSlave(){
+        return userDao.queryAllWithSlave();
+    }
+
+    @DataSource(name = "data-master")
+    @GetMapping("queryAllWithMaster")
+    public Object queryAllWithMaster(){
+        return userDao.queryAllWithMaster();
     }
 }
--- src/main/java/com/xiaobin/web/dao/UserDao.java
+++ src/main/java/com/xiaobin/web/dao/UserDao.java
@@ -3,12 +3,14 @@ package com.xiaobin.web.dao;
 import com.xiaobin.base.dao.BaseDao;
 import com.xiaobin.web.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author kevin
  * @since 2020/4/2 1:20 PM
  */
 @Mapper
+@Repository
 public interface UserDao extends BaseDao<User> {
     User queryAllWithMaster();
     User queryAllWithSlave();
